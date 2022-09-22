23dfa11fceef05345e2ffe2dd58529b729797841
Steele <stablesc@mail.uc.edu>
2022-09-22 17:40:05 -0400

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int age = 19;
+
+        if(age >= 21)
+        {
+            System.out.println("You get a wrist band!");
+        }
     }
 }
