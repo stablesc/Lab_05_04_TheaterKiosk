0af0566861531702ea211be9f92009b31b4ad007
Steele <stablesc@mail.uc.edu>
2023-02-28 01:24:30 -0500

    final commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,13 @@
+public class TheaterKiosk {
+    public static void main(String[] args)
+    {
+        System.out.println("Enter your age : ");
+        //simulate input
+        int age = 25;
+
+        if(age >= 21)
+        {
+            System.out.println("You get a wristband!");
+        }
+    }
+}
